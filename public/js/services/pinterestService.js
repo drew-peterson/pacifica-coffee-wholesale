9c967e44c221530b79b739ac0d2217022d821b98
weeznog <drew.m.peterson@gmail.com>
2016-02-02 16:17:09 -0800

    Update photos for about, included new pinterest board

--- public/js/services/pinterestService.js
+++ public/js/services/pinterestService.js
@@ -4,7 +4,8 @@ angular.module('Pinterest')
 
 .factory('pinterestService', function($http){
 
-  var boardUrl = 'https://api.pinterest.com/v1/boards/543176473746760468/pins/?access_token=ATqC1gzDTvxL0zf-1wfyp-SdFCe3FCx7yHOmO5hC0EfPzMArmQAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage';
+// 543176473746760468 //- old board
+  var boardUrl = 'https://api.pinterest.com/v1/boards/419538590226392216/pins/?access_token=ATqC1gzDTvxL0zf-1wfyp-SdFCe3FCx7yHOmO5hC0EfPzMArmQAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage';
 
   var pins = [];
 
