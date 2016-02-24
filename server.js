29752f9b0a58a1a9301e6e82f4fa8ade9e8a882a
weeznog <drew.m.peterson@gmail.com>
2016-02-24 11:02:33 -0800

    remove dist

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var port = process.env.PORT || 3000;
 app.use(require('prerender-node').set('prerenderToken', 'EDCiXmecUrcmqYK7hZ5M'));
 
 app.use(compression()); //gzip compression
-app.use(express.static(__dirname + '/dist/public')); // add /dist/ for production
+app.use(express.static(__dirname + '/public')); // add /dist/ for production
 
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
