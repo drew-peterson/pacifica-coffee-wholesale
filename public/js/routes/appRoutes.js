c0fd17dc406128efb0ec972af0848bec83f8b85b
weeznog <drew.m.peterson@gmail.com>
2016-02-24 13:03:03 -0800

    ui view is working

--- public/js/routes/appRoutes.js
+++ public/js/routes/appRoutes.js
@@ -5,14 +5,39 @@ angular.module('appRoutes', [])
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-    .state('index', {
-      url: '/',
-      controller: 'MainCtrl',
-      templateUrl: '../views/home.html',
+    .state('root',{
+      url: '',
+      abstract: true,
+      views: {
+        'nav': {
+          templateUrl: '../views/nav/nav.html'
+        },
+        'footer': {
+          templateUrl: '../views/footer/footer.html'
+        }
+      }
     })
+    .state('root.home', {
+      url: '/',
+      views: {
+        'container@': {
+          templateUrl: '../views/home.html'
+        }
+      }
+    })
+
+    .state('root.about',{
+      url: '/about',
+      views: {
+        'container@': {
+          templateUrl: '../views/about/about.html'
+        }
+      }
+    })
+   
 
     // GoogleBot SEO
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
 
-  });
+});
