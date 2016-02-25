56df86aa5de64ff9761286bc4743b0b6d775f086
weeznog <drew.m.peterson@gmail.com>
2016-02-25 13:40:41 -0800

    started adding angular events

--- public/js/controllers/navCtrl.js
+++ public/js/controllers/navCtrl.js
@@ -1,10 +1,42 @@
 angular.module('NavCtrl',[])
 
 .controller('NavCtrl', function($scope){
+	$scope.actve;
+	$scope.hover
 
+	$scope.checkNav = function(event){
+		var el = document.getElementById('sideNav').classList;
+		var h = $scope.hover;
+		var a = $scope.active;
+		var type = event.type
 
-	$scope.openNav = function(){
-		console.log('click')
-		$scope.active = true;
+		if(type == 'mouseover'){
+			// mouse over menu
+			if(h){
+				$scope.hover = false;
+				$scope.active = true;
+			}
+
+		}else{
+		// click event
+		$scope.hover = false;
+
+			// 
+			if(!a){
+				// close menu
+				$scope.active = !$scope.active;
+			}
+		}
+
+		// $scope.$watch('el', function(newValue, oldValue){
+		// 	console.log('new: ' + newValue); 
+		// 	console.log('old: ' + oldValue);
+		// })
+
+		console.log(el);
+		console.log('active: ' + $scope.active);
+		console.log('hover: ' + $scope.hover);
 	}
+
+
 })
