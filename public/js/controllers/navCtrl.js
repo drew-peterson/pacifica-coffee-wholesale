b2a05987ec74dc724aa97a885a242980cd44fdf8
weeznog <drew.m.peterson@gmail.com>
2016-02-25 19:31:26 -0800

    made adjustment to mobile menu clicks

--- public/js/controllers/navCtrl.js
+++ public/js/controllers/navCtrl.js
@@ -9,13 +9,22 @@ angular.module('NavCtrl',[])
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
-			var body = $('body');
-
+	
 			element.bind('click', function(){
 				var isMobile = event.sourceCapabilities.firesTouchEvents;
 				if(isMobile){
+					
 					$('#sideNav').removeClass('hover');
-					$('#sideNav').toggleClass('active');
+					$('#sideNav').toggleClass('active');	
+
+					$('#main').on('click', function(){
+						$('#sideNav').removeClass('hover');
+						$('#sideNav').removeClass('active');
+
+						// remove listner
+						$(this).off()						
+						
+					})
 				}
 				
 				
