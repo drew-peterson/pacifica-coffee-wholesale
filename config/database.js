21e2e763ee7360b39396a2777b854a87f21607b0
weeznog <drew.m.peterson@gmail.com>
2016-03-05 13:56:56 -0800

    add uri maybe

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var URI = process.env.MONGOLABS_URI || 'mongodb://localhost/pacifica-coffee';
 
 // set up mongo database run with mongod
-mongoose.connect('mongodb://localhost/pacifica-coffee', function(err){
+mongoose.connect(URI, function(err){
 	if(err){
 		console.log("failed to log MongoDB");
 		console.log(err);
 	}else{
-		console.log("drew -- MongoDB Running Successfully!");
+		console.log("drew -- MongoDB Running Successfully!"); 
 	}
 })
