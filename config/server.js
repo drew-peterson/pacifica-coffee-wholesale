fded16355c3b200043fa4c7763dc4d5ec7f0219c
weeznog <drew.m.peterson@gmail.com>
2016-03-04 17:09:07 -0800

    make config folder sever and routes

--- config/server.js
+++ config/server.js
@@ -4,25 +4,21 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 var compression = require('compression');
-var fs = require('fs');
 
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(compression()); //gzip compression
+app.use(require('prerender-node').set('prerenderToken', 'EDCiXmecUrcmqYK7hZ5M')); // prerender.io
 
 var port = process.env.PORT || 3000;
 
-// prerender.io
-app.use(require('prerender-node').set('prerenderToken', 'EDCiXmecUrcmqYK7hZ5M'));
 
-app.use(compression()); //gzip compression
-app.use(express.static(__dirname + '/public')); // add /dist/ for production
+app.use('/', express.static('public')); // add /dist/ for production
 
-// routes ==================================================
-// require('./app/routes')(app); // configure our routes
-
-app.listen(port);
-console.log("drew -- listening on node server port 3000");
+app.listen(port, function(){
+	console.log("drew -- listening on node server port 3000");
+}); 
 
 // expose app
 module.exports = app;
