dd2046eb07515e12f0a01c81968840e663e8302d
weeznog <drew.m.peterson@gmail.com>
2016-03-18 15:32:53 -0700

    notification bar up and running

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -476,6 +476,39 @@ angular.module('CoffeeCtrl')
 	}
 }) 
 angular.module('CoffeeCtrl')
+.directive('notificationBar',function($timeout){
+	return{
+		scope:true,
+		replace: true,
+		link: function(scope,elem,attrs){
+
+			// item updated ========
+			$('#coffeeBag').on('change', 'select', function(){
+				elem.addClass('showNotification');
+				elem.find('.update').show();
+				$timeout(function() {
+					elem.removeClass('showNotification');
+					elem.find('.update').hide();
+				}, 2000);
+			})
+
+
+
+			// how show adde item =================
+			$('.card-wrapper').on('click','.coffee-card .add', function(){
+
+				elem.addClass('showNotification');
+				elem.find('.add').show();
+				$timeout(function() {
+					elem.removeClass('showNotification');
+					elem.find('.add').hide();
+				}, 2000);
+			});
+		},
+		templateUrl: 'views/coffee/notificationBar.html'
+	}
+})
+angular.module('CoffeeCtrl')
 .directive('showFilter',function(){
 	return {
 		scope: true,
