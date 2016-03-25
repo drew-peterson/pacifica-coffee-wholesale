223a956a72b664317c1b2480643e9659923bce6a
weeznog <drew.m.peterson@gmail.com>
2016-03-25 13:46:08 -0700

    added mask to nav

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -177,41 +177,58 @@ angular.module('NavCtrl',[])
 			var active;
 			var navBtn = elem.find('.nav-open');
 			var home = navBtn.parent().find('.title');
+			var mask = $('.mask-overlay');
 			
 			// Mobile Menu
 			navBtn.on('click',function(){
 				var sideNav = elem.find('#sideNav');
 				var link = sideNav.find('.link');
-				var iconText = elem.find('#sideNav .iconText'); 
+				var iconText = elem.find('#sideNav .iconText');
 
 				if(!active){
 					sideNav.addClass('showSideNavM');
-					iconText.addClass('showIconText'); 
+					iconText.addClass('showIconText');
+					mask.addClass('show');
 					active = true;
 				}else{
 					sideNav.removeClass('showSideNavM');
 					sideNav.removeClass('showSideNavD');
 					iconText.removeClass('showIconText'); 
+					mask.removeClass('show');
 					active = false;
+					// removeAll(sideNav, iconText, mask);
 				}
 				// on home btn click remove everything
 				home.on('click',function(){
 					sideNav.removeClass('showSideNavM');
 					sideNav.removeClass('showSideNavD');
-					iconText.removeClass('showIconText'); 
+					iconText.removeClass('showIconText');
+					mask.removeClass('show'); 
 					active = false;
+					// removeAll(sideNav, iconText, mask);
 				})
 				// set active to false on link click;
 				link.on('click', function(){
+					mask.removeClass('show');
 					active = false;
+				})
+				mask.on('click',function(){
+					sideNav.removeClass('showSideNavM');
+					sideNav.removeClass('showSideNavD');
+					iconText.removeClass('showIconText');
+					mask.removeClass('show'); 
+					active = false;
+					// removeAll(sideNav, iconText, mask);
 				})
 			})
 
 			navBtn.on('mouseenter',function(){
 				var sideNav = elem.find('#sideNav');
 				var iconText = $('#sideNav .iconText');
+				var mask = $('.mask-overlay');
 				
 				sideNav.addClass('showSideNavD');
+				mask.addClass('show');
 
 				sideNav.on('mouseenter',function(){
 					iconText.addClass('showIconText');
@@ -219,6 +236,7 @@ angular.module('NavCtrl',[])
 				})
 
 				sideNav.on('mouseleave',function(){
+					mask.removeClass('show');
 					sideNav.removeClass('showSideNavD');
 					sideNav.removeClass('showSideNavM');
 					iconText.removeClass('showIconText');
@@ -231,6 +249,15 @@ angular.module('NavCtrl',[])
 				var sideNav = elem.find('#sideNav');
 				sideNav.removeClass('showSideNavD');
 			})
+
+			var removeAll = function(sideNav, iconText, mask){
+				console.log('removeAll')
+				sideNav.removeClass('showSideNavM');
+				sideNav.removeClass('showSideNavD');
+				iconText.removeClass('showIconText');
+				mask.removeClass('show'); 
+				active = false;
+			}
 		},
 		templateUrl: 'views/nav/nav.html'
 	} 
