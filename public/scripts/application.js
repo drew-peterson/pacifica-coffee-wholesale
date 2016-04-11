cb6f9b3510e7f08f913e8f1984d04725471e12c1
weeznog <drew.m.peterson@gmail.com>
2016-04-11 13:19:35 -0700

    add show close on x wot contact moda

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1071,16 +1071,36 @@ angular.module('HomeCtrl').directive('videoHero', function(){
 	}
 }); 
 angular.module('NavCtrl')
-.directive('contactModal', function(){
+.directive('contactModal', function($animate){
 	return {
 		replace: true,
 		restrict: 'E',
 		templateUrl: 'views/nav/contactModal.html',
 		link: function(scope, elem, attrs){
 
+			var mask = $('.mask-overlay');
+
 			$('.subNav.contact').on('click',function(){
-				console.log('click')
-			})
+				var modal = $('#nav .baseModal');
+				var nav = $('#sideNav');
+
+
+				scope.$apply(function(){
+					$animate.addClass(modal, 'show');
+					$animate.removeClass(mask, 'show');
+					$animate.removeClass(nav, 'showSideNavM');
+				})
+
+
+				$('.baseModal .close').on('click', function(){
+					
+					$('body').css('overflow', 'initial');
+					scope.$apply(function(){
+						$animate.removeClass(modal, 'show');
+					})
+				})
+				
+			});
 		}
 	}  
 })
