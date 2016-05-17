6c1e65ee8892bf0c36344ab40733930da6ce9f39
weeznog <drew.m.peterson@gmail.com>
2016-05-17 11:04:45 -0700

    basic video load...

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1133,23 +1133,10 @@ angular.module('HomeCtrl').directive('videoHero', function(){
 			var video = document.getElementById('bgvid');
 			var chrome = navigator.appVersion.indexOf('Chrome');
 
-			// if Chrome Else
-			// if(chrome >= 0){
-			// 	video.addEventListener('canplay', function(){},false);
-			// 	video.addEventListener('progress', function(){
-			// 		// if(Math.round(video.buffered.end(0)) === 8){
-			// 		// 	video.play(); 
-			// 		// }else{
-			// 		// 	console.log('buffering')
-			// 		// }
-			// 	});
-			// 	// video.play();
-			// }else{
-				video.oncanplaythrough = function() { 
-    				video.play(); 
-				};
+			video.oncanplaythrough = function() { 
+				video.play(); 
+			};
 				
-			// }
 		},
 		templateUrl: "views/home/youtube.html" 
 	}
